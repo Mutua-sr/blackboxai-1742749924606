45b6e2ecdf23ff686f703be6c4deda45fe2aaf09
Mutua Meshack <128325049+Mutua-sr@users.noreply.github.com>
2025-03-23 20:15:13 +0300

    Add blackboxai-1742693114516/blackboxai-1742671172553/eduapp/frontend/jest.config.js 1742749940616

---
+++ blackboxai-1742693114516/blackboxai-1742671172553/eduapp/frontend/jest.config.js
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: './tsconfig.json'
+    }]
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironmentOptions: {
+    url: 'http://localhost'
+  }
+};
